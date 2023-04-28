c125544750930b4b29b7b5686c42c6de59690446
Georgy <goshagang@mail.ru>
2023-04-28 20:05:16 +0500

    price is updated

--- src/components/GET_API.jsx
+++ src/components/GET_API.jsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-/*
-Делаю API запрос на сайт cryptocompare, чтобы получить актуальный курс криптовалют
-*/
-
 const API_URL = 'https://min-api.cryptocompare.com/data/pricemulti';
+const INTERVAL_TIME = 1000;
 
 function GET_API() {
+  const [data, setData] = useState({});
+
   useEffect(() => {
+    const interval = setInterval(() => {
+      fetchData();
+    }, INTERVAL_TIME);
+    return () => clearInterval(interval);
+  }, []);
+
+  const fetchData = () => {
     axios.get(API_URL, {
       params: {
         fsyms: 'BTC,ETH,LTC,BSW,CAKE',
@@ -17,12 +23,12 @@ function GET_API() {
     })
     .then(response => {
       const data = response.data;
-      console.log(data);
+      setData(data);
     })
     .catch(error => {
       console.error(error);
     });
-  }, []);
+  };
 }
 
 export default GET_API;

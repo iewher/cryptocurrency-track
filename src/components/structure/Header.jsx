2f7183e5822bde78a5b043de8670fd75b694edea
Georgy <goshagang@mail.ru>
2023-04-29 11:38:11 +0500

    added and styled header

--- src/components/structure/Header.jsx
+++ src/components/structure/Header.jsx
@@ -4,7 +4,20 @@ import '../style/style.css'
 export default function Header() {
   return (
     <div className='Header'>
-        Header
+        <div className='name'>
+          Cryptocurrency track
+        </div>
+        <div className='buttons'>
+          <button className='button-home'>Главная</button>
+          <button className='button-top-100'>Топ-100</button>
+          <button className='button-check'>Отслеживаемое</button>
+        </div>
+        <div className='input'>
+          <input type='text' placeholder='Название монеты' className='search'></input>
+        </div>
+        <div className='settings'>
+          <button className='button-setting'>Setting</button>
+        </div>
     </div>
   )
 }

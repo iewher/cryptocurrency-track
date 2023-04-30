68f898f090e1edac1b4a334b51cf375a6eab65dd
Georgy <goshagang@mail.ru>
2023-04-30 12:25:05 +0500

    routing configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import Home from './structure/pages/page-home/Home';
 import './style/style.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+
+import Home from './structure/pages/page-home/Home';
+import Top100 from './structure/pages/page-top-100/Top100';
+import Check from './structure/pages/page-check/Check';
 
 function App() {
   return (
-    <div className='container'>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className='container'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/top-100' element={<Top100 />} />
+          <Route path='/check' element={<Check />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 import './style/header-style.css'
+
+import Home from '../../structure/pages/page-home/Home'
+import Top100 from '../../structure/pages/page-top-100/Top100'
+import Check from '../../structure/pages/page-check/Check'
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -7,17 +12,23 @@ export default function Header() {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-
+  
   return (
     <div className='Header'>
         <div className='name'>
           Cryptocurrency track
         </div>
-        <div className='buttons'>
-          <button className='button-home'>Главная</button>
-          <button className='button-top-100'>Топ-100</button>
-          <button className='button-check'>Отслеживаемое</button>
-        </div>
+            <div className='buttons'>
+              <Link to='/'>
+                <button className='button-home'>Главная</button>
+              </Link>
+              <Link to='/top-100'>
+                <button className='button-top-100'>Топ-100</button>
+              </Link>
+              <Link to='/check'>
+                <button className='button-check'>Отслеживаемое</button>
+              </Link>
+            </div>
         <div className='input'>
           <input type='text' placeholder='Название монеты' className='search'></input>
         </div>

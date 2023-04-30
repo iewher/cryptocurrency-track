a9f18a19012de32b81695206ede08f104e68952f
Georgy <goshagang@mail.ru>
2023-04-30 12:25:21 +0500

    removed unnecessary

--- src/structure/pages/page-home/Home.jsx
+++ src/structure/pages/page-home/Home.jsx
@@ -3,7 +3,6 @@ import './style/home-style.css';
 import Header from '../../../components/Header/Header'
 import Bottom from '../../../components/Bottom/Bottom'
 
-
 export default function Home() {
   return (
     <div className='block'>

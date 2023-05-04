46b91b4d093dc8c844c569e70e2f0b083a983643
iewher <goshagang@mail.ru>
2023-05-04 17:21:18 +0500

    changed search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 import './style/search-style.css'
 
 export default function Search() {
   return (
       <div className='input'>
         <input type='text' placeholder='Название монеты' className='search'></input>
+        <button className='button-search'>Поиск</button>
       </div>
   )
 }

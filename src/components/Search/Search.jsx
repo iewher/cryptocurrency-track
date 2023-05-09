cc3005da2d18ebf79481bd222dcb059dc9aee69f
iewher <goshagang@mail.ru>
2023-05-09 19:48:32 +0500

    changing the meaning of search operation

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,61 +1,22 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './style/search-style.css';
-import axios from 'axios';
-import Coin from '../../structure/pages/page-coin/Coin'
+import GET_API_SEARCH from '../GET_API/GET_API_SEARCH'
+import SearchForm from './Search-form';
+import Coin from '../../structure/pages/page-coin/Coin';
 
-export const API_URL = 'https://min-api.cryptocompare.com/data/pricemultifull';
 export const INTERVAL_TIME = 1000;
-export const LOCAL_STORAGE_KEY = 'cryptoData';
-
-const fetchData = async (coin, currency) => {
-  try {
-    const response = await axios.get(API_URL, {
-      params: {
-        fsyms: coin,
-        tsyms: currency,
-      },
-    });
-    const data = response.data;
-    // console.log(data);
-    return data;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-};
-
-const SearchForm = ({ searchCoin, handleInputChange, handleSearch }) => {
-  return (
-    <div className="header-search">
-      <input
-        type="text"
-        placeholder="Название монеты"
-        className="search-input"
-        value={searchCoin}
-        onChange={handleInputChange}
-      />
-      <Link to="/coin">
-        <button className="search-button" onClick={handleSearch}>
-          Поиск
-        </button>
-      </Link>
-    </div>
-  );
-};
 
 export default function Search() {
   const [data, setData] = useState({});
   const [searchCoin, setSearchCoin] = useState('');
   const [intervalId, setIntervalId] = useState(null);
 
-  // console.log(data);
-
   const startInterval = () => {
     const id = setInterval(() => {
-      fetchData(searchCoin, 'USD')
+      GET_API_SEARCH(searchCoin, 'USD')
         .then(data => {
           setData(data);
+          <Coin data={data} />
+          // console.log(data);
         })
         .catch(error => {
           console.error(error);
@@ -77,7 +38,7 @@ export default function Search() {
   };
 
   const handleSearch = () => {
-    fetchData(searchCoin, 'USD')
+    GET_API_SEARCH(searchCoin, 'USD')
       .then(data => {
         setData(data);
         if (!intervalId) {
--- src/structure/pages/page-coin/Coin.jsx
+++ src/structure/pages/page-coin/Coin.jsx
@@ -1,15 +1,34 @@
-import React, {useState} from 'react'
+import React from 'react';
 import './style/coin-style.css';
 
 export default function Coin({ data }) {
-  // console.log(data)
+  console.log(data);
   return (
     <div className='coin'>
-        <div className='coin-body'>
-            <div className='coin-table'>
-              
-            </div>
-          </div>
+      <div className='coin-body'>
+        <div className='coin-table'>
+          {data && Object.keys(data).length > 0 ? (
+            <table>
+              <thead>
+                <tr>
+                  <th>Coin</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map(coin => (
+                  <tr key={coin.id}>
+                    <td>{coin.DISPLAY.USD.FROMSYMBOL}</td>
+                    <td>{coin.RAW.USD.PRICE}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p>No data available</p>
+          )}
+        </div>
+      </div>
     </div>
-  )
+  );
 }

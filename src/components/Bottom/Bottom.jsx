cb8fb747203c9dabd507efd40c5aec39f1532c5f
iewher <goshagang@mail.ru>
2023-05-11 16:05:03 +0500

    added icons git and telegram, added link on icons

--- src/components/Bottom/Bottom.jsx
+++ src/components/Bottom/Bottom.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './style/bottom-style.css'
+import {AiFillGithub} from 'react-icons/ai'
+import {BsTelegram} from  'react-icons/bs'
+
+const URL_GIT = 'https://github.com/iewher/cryptocurrency-track';
+const URL_TG = 'https://t.me/iewher';
 
 export default function Bottom() {
   return (
     <div className='bottom'>
-      <div className='bottom-rights'>© 2023 Cryptocurrency track. All rights reserved</div>
+      <div className='bottom-rights'>
+        © 2023 Cryptocurrency track. All rights reserved
+      </div>
+      <Link to={URL_GIT}>
+        <button className='bottom-git'>
+          <AiFillGithub className='icons'/>
+        </button>
+      </Link>
+      <Link to={URL_TG}>
+        <button className='bottom-telegram'>
+          <BsTelegram className='icons'/>
+        </button>
+      </Link>
     </div>
   )
 }

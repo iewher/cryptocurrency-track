1a0927fb7e72856c8ef18bad7530b823cb7fc6c8
iewher <goshagang@mail.ru>
2023-05-15 23:39:45 +0500

    corrected code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,18 +9,18 @@ export default function Header() {
         <div className='-header-name'>
           Cryptocurrency track
         </div>
-            <div className='header-buttons'>
-              <Link to='/'>
-                <button className='header-button-home'>Главная</button>
-              </Link>
-              <Link to='/top-100'>
-                <button className='header-button-top-100'>Топ-100</button>
-              </Link>
-              <Link to='/check'>
-                <button className='header-button-check'>Отслеживаемое</button>
-              </Link>
-            </div>
-            <ShowSearch />
+        <div className='header-buttons'>
+          <Link to='/'>
+            <button className='header-button-home'>Главная</button>
+          </Link>
+          <Link to='/top-100'>
+            <button className='header-button-top-100'>Топ-100</button>
+          </Link>
+          <Link to='/check'>
+            <button className='header-button-check'>Отслеживаемое</button>
+          </Link>
+        </div>
+        <ShowSearch />
     </div>
   )
 }

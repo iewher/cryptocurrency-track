baed4d46bcaf80598a6db587c0bdc143550cfd96
iewher <goshagang@mail.ru>
2023-06-08 23:58:48 +0500

    added log in and sign up

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style/header-style.scss';
 import { AiOutlineUser } from 'react-icons/ai';
 
@@ -6,6 +6,27 @@ import { Link } from 'react-router-dom';
 import { ShowSearch } from '../Search/Search';
 
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [showAuth, setShowAuth] = useState(false);
+
+  const handleToggleMenu = () => {
+    setShowMenu(!showMenu);
+  }
+
+  const handleAuthLogin = () => {
+    setShowLogin(true);
+  }
+
+  const handleAuthMenu = () => {
+    setShowAuth(true);
+  }
+
+  const closeMenu = () => {
+    setShowAuth(false);
+    setShowLogin(false);
+  }
+
   return (
     <div className='header'>
         <div className='header-name'>
@@ -24,7 +45,32 @@ export default function Header() {
         </div>
         <ShowSearch />
         <div className="header-user">
-          <button className='header-settings'><AiOutlineUser /></button>
+          <button className='header-settings' onClick={handleToggleMenu}><AiOutlineUser /></button>
+          {showMenu && (
+            <div className='header-menu'>
+              <p>Меню</p>
+              <button onClick={handleAuthLogin}>Авторизоваться</button>
+              <button onClick={handleAuthMenu}>Зарегистрироваться</button>
+            </div>
+          )}
+          {showAuth && (
+            <div className='header-auth'>
+              <button onClick={closeMenu} className='header-auth-close'>Закрыть</button>
+              <p>Sign up</p>
+              <input placeholder='Email' type='email'></input>
+              <input placeholder='Password' type='password'></input>
+              <button>Создать профиль</button>
+            </div>
+          )}
+          {showLogin && (
+            <div className='header-login'>
+              <button onClick={closeMenu} className='header-auth-close'>Закрыть</button>
+              <p>Log in</p>
+              <input placeholder='Email' type='email'></input>
+              <input placeholder='Password' type='password'></input>
+              <button>Войти</button>
+            </div>
+          )}
         </div>
     </div>
   )
